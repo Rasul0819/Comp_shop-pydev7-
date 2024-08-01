6245270d50a818cd3e3876f47768e28233b32938
Rasul <104918755+Rasul0819@users.noreply.github.com>
2024-08-01 13:04:04 +0500

    product_detail

--- shop/urls.py
+++ shop/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 
 urlpatterns =[
-    path('',views.homepage,name='homepage')
+    path('',views.homepage,name='homepage'),
+    path('product/<int:id>',views.product_detail,name='detail'),
 ]
--- shop/views.py
+++ shop/views.py
@@ -1,7 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render,get_object_or_404
 from . import models
 
 def homepage(request):
     products = models.Product.objects.all()
 
     return render(request,'home.html',{'products':products})
+
+
+def product_detail(request,id):
+    product = get_object_or_404(models.Product,id=id)
+
+    return render(request,'product_detail.html',{'product':product})
+
